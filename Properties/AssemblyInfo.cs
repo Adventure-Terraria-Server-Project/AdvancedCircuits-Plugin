46b13c9bb7b27d68cde8049ff94f193bf2f359a2
CoderCow <CoderCow@gmx.de>
2017-05-09 02:11:31 +0200

    chore(version): tick plugin version 1.6.2

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("ECC96E2F-0CBF-4B0A-A583-15658D5091DA")]
 
-[assembly: AssemblyVersion("1.6.1.0")]
-[assembly: AssemblyFileVersion("1.6.1.0")]
+[assembly: AssemblyVersion("1.6.2.0")]
+[assembly: AssemblyFileVersion("1.6.2.0")]
 
-[assembly: AssemblyInformationalVersion("1.6.1+4.Branch.master.Sha.842ee0b0c4a1944cbfa9a10798e98d3aef7cabf0")]
+[assembly: AssemblyInformationalVersion("1.6.2+5.Branch.master.Sha.c4f1e8d8e4d2317a00ad0f14bd246be04ca5ba19")]
